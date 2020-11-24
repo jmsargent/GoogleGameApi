6b424dd05a32aca6598f61dca44a73e8fdaa97fb
Jonathan Sargent <Jmsargent94@gmail.com>
2020-11-24 23:20:58 +0100

    First Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Main {
+
+    public static void main(String[] args) {
+        FirefoxDriver firefoxDriver = new FirefoxDriver();
+        System.out.println("hej");
+    }
+}
